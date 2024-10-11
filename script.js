2002b91d2682471ef8c883e1dc8e9a118086c949
Jul$ <yureusu9@gmail.com>
2024-10-11 21:05:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,185 @@
+function openNav() {
+    document.getElementById("side-nav").style.width = "93%";
+    document.getElementById("side-nav").style.display = "flex";
+}
+function closeNav() {
+    document.getElementById("side-nav").style.width = "0%";
+    document.getElementById("side-nav").style.display = "none";
+}
+var imageTracker = 'black';
+function espressoheart() {
+    var image = document.getElementById('espressoheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
+function latteheart() {
+    var image = document.getElementById('latteheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
+function macchiatoheart() {
+    var image = document.getElementById('macchiatoheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
+function cappheart() {
+    var image = document.getElementById('cappheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
+function chocomilkheart() {
+    var image = document.getElementById('chocomilkheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
+function taromilkheart() {
+    var image = document.getElementById('taromilkheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
+function matchamilkheart() {
+    var image = document.getElementById('matchamilkheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
+function mochamilkheart() {
+    var image = document.getElementById('mochamilkheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
+function mochaprafheart() {
+    var image = document.getElementById('mochaprafheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
+function taroprafheart() {
+    var image = document.getElementById('taroprafheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
+function chocoprafheart() {
+    var image = document.getElementById('chocoprafheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
+function matchaprafheart() {
+    var image = document.getElementById('matchaprafheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
+function classichalohaloheart() {
+    var image = document.getElementById('classichalohaloheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
+function tarohalohaloheart() {
+    var image = document.getElementById('tarohalohaloheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
+function chocohalohaloheart() {
+    var image = document.getElementById('chocohalohaloheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
+function berryhalohaloheart() {
+    var image = document.getElementById('berryhalohaloheart');
+    if(imageTracker == 'black'){
+        image.src = 'heart-red.png';
+        imageTracker = 'red';
+    }
+    else{
+        image.src = 'heart.png';
+        imageTracker = 'black';
+    }
+}
